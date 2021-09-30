4c1a06a8e7aa30d66018fee50b35ec2734fdd7c7
puterifarah <puterifarah26@gmail.com>
2021-09-30 10:59:59 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+<head> 
+    <meta charset="UTF-8"> 
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>Latihan Kedua</title> 
+     
+</head> 
+<body>
+    <h1>Hello world</h1>
+
+    <?php
+    $nama = 'Puteri Farah Diba Natasha';
+    $umur = 19 + 1;
+    
+
+    echo " Saya $nama berumur $umur tahun";
+    ?>
+
+</body>
+</html>
